3e5783e76708da026913053908352fcac9121858
Brucearmstrong007 <brucearmstrongofficial@gmail.com>
2023-03-16 16:10:10 +0530

    Update

--- apps/client-e2e/src/support/app.po.ts
+++ apps/client-e2e/src/support/app.po.ts
@@ -3,7 +3,7 @@ export const passwordTag = () => getTag('#password');
 export const confirmPasswordTag = () => getTag('#confirmPassword');
 export const submitTag = () => getTag('#submit');
 export const imageURLTag = () => getTag('#image')
-export const titleTag = () => title();;
+export const titleTag = () => title();
 
 
 export const getTag = (selector: string) => cy.get(selector).should('exist').should('be.visible');
